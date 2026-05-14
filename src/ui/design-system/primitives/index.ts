7720ca3d1357e75de074c1ddd9d4ba174a43a6e0
Stefano Valloncini <stefano.valloncini@gmail.com>
2026-05-14 21:33:52 +0200

    refactor(design): export Money, MetricBlock, Tile from barrel

--- src/ui/design-system/primitives/index.ts
+++ src/ui/design-system/primitives/index.ts
@@ -1,8 +1,7 @@
 export { Button } from "./Button.tsx";
-export type { ButtonProps } from "./Button.tsx";
 export { Field } from "./Field.tsx";
-export type { FieldProps } from "./Field.tsx";
 export { Select } from "./Select.tsx";
-export type { SelectOption, SelectProps } from "./Select.tsx";
 export { Stack } from "./Stack.tsx";
-export type { StackProps } from "./Stack.tsx";
+export { Money } from "./Money.tsx";
+export { MetricBlock } from "./MetricBlock.tsx";
+export { Tile } from "./Tile.tsx";
