5c424867eb14b7d7da01aa308d23288f0839d162
stefanovalloncini <stefanovalloncini@gmail.com>
2026-09-21 00:48:48 +0200

    chore: remove unused tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
